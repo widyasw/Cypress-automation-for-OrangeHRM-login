0bf07ebd39a349574a48846a7e750f904a42607f
Widya <99782577+widyasw@users.noreply.github.com>
2026-01-15 10:38:47 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://opensource-demo.orangehrmlive.com',
-    supportFile: 'cypress/support/e2e.js'
+    supportFile: 'cypress/support/e2e.js',
+
+    env: {
+      API_KEY: 'reqres_b31ce4b07cd74c8789d5a44448118aa2'
+    }
   }
 })
